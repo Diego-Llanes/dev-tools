32a55b337e7d2d4bc1b38be53bf7bdba90245f8b
Llanes, Diego <diego@pnnl.gov>
2024-07-10 09:24:28 -0700

    Started using Wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,38 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then config = wezterm.config_builder() end
+
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font = wezterm.font(
+	'JetBrains Mono',
+	{ weight = 'Bold', italic = true }
+)
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+config.use_fancy_tab_bar = false
+config.window_close_confirmation = 'NeverPrompt'
+
+wezterm.on("format-tab-title",
+    function(tab, tabs, panes, config, hover, max_width)
+        local pane_title = tab.active_pane.title
+        local user_title = tab.active_pane.user_vars.panetitle
+
+        if user_title ~= nil and #user_title > 0 then
+	    pane_title = user_title
+        end
+
+        return {
+	    { Text = " " .. pane_title .. " " },
+        }
+    end
+)
+
+return config
